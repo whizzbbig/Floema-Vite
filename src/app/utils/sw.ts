f07ca60a69234d8f1346cfac696fbcc7bed56ffd
cadupuy <charlesantoine.dupuy@gmail.com>
2023-08-14 18:28:00 +0200

    :construction: wip service worker

--- src/app/utils/sw.ts
+++ src/app/utils/sw.ts
@@ -1,3 +1,11 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/src/app/service-worker.js');
+  navigator.serviceWorker
+    .register('/src/app/service-worker.js')
+
+    .then(function (registration) {
+      console.log('Service Worker OK : ', registration.scope);
+    })
+    .catch(function (error) {
+      console.log('Service Worker Failed :', error);
+    });
 }
