ee12eb3845cb8f851ea387651d45c6bc7f61b04e
cadupuy <charlesantoine.dupuy@gmail.com>
2023-08-25 13:56:11 +0200

    :memo: update readme comments

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,3 +1,4 @@
+// Essential module imports for the configuration
 const path = require('path');
 const htmlmin = require('html-minifier');
 const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
@@ -5,16 +6,24 @@ const { VitePWA } = require('vite-plugin-pwa');
 const glslifyPlugin = require('vite-plugin-glslify').default;
 
 module.exports = function (eleventyConfig) {
+  // Configuring the Eleventy server to run on port 3000
   eleventyConfig.setServerOptions({
     port: 3000,
   });
 
+  // Integrate Vite with Eleventy using the Eleventy Vite Plugin
   eleventyConfig.addPlugin(EleventyVitePlugin, {
+    // Specify the directory where Vite-specific temporary files will be stored
     tempFolderName: '.11ty-vite',
+    // Options tailored for the Vite build tool
     viteOptions: {
+      // Directory to serve static assets from
       publicDir: 'public',
+      // Set the root directory for Vite
       root: 'src',
+      // List of Vite plugins to use
       plugins: [
+        // PWA (Progressive Web App) settings using VitePWA plugin
         VitePWA({
           injectRegister: 'script',
           registerType: 'autoUpdate',
@@ -23,10 +32,13 @@ module.exports = function (eleventyConfig) {
             globPatterns: ['**/*.{js,css,html,png,jpg,svg,woff,woff2}'],
           },
         }),
+        // GLSL (OpenGL Shading Language) support using glslifyPlugin
         glslifyPlugin(),
       ],
 
+      // Module resolve options
       resolve: {
+        // Create alias for directories, simplifying import paths
         alias: {
           '@styles': path.resolve('.', '/src/styles'),
           '@app': path.resolve('.', '/src/app'),
@@ -42,14 +54,14 @@ module.exports = function (eleventyConfig) {
     },
   });
 
-  // Copy files for asset pipeline
+  // Specify directories and files that should bypass Eleventy's processing and be copied "as-is"
   eleventyConfig.addPassthroughCopy('public');
   eleventyConfig.addPassthroughCopy('src/app');
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/styles');
   eleventyConfig.setServerPassthroughCopyBehavior('copy');
 
-  // Minify HTML
+  // Minify HTML files before writing to the output directory
   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
     if (outputPath && outputPath.endsWith('.html')) {
       const minified = htmlmin.minify(content, {
@@ -62,10 +74,13 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 
+  // Define input and output directories for Eleventy, set passthrough copy, and set template engine to Pug
   return {
     dir: {
       input: 'src/views',
       output: '_site',
+      includes: '_includes',
+      data: '_data',
     },
     passthroughFileCopy: true,
     htmlTemplateEngine: 'pug',
